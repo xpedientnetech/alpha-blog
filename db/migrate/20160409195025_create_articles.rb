da2107ae746f224727bdb714c8fc154458b9ea22
humbe1014 <xpedientnetechservices@gmail.com>
2016-04-09 20:30:54 +0000

    rebuilt migration files to include timestamps

--- db/migrate/20160409195025_create_articles.rb
+++ db/migrate/20160409195025_create_articles.rb
@@ -2,6 +2,9 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :title
+      t.text :description
+
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408063053) do
+ActiveRecord::Schema.define(version: 20160409195025) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "create_at"
-    t.datetime "update_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
